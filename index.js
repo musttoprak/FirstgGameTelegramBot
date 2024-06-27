bb19a24361a991a2b2a6ba63a764bba7b105d24e
musttoprak <musttoprakk@gmail.com>
2024-06-27 13:52:17 +0300

    add node module and udate index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const bot = new TelegramBot(TOKEN, {
     polling: true
 });
 const port = process.env.PORT || 5000;
-const gameName = "firstgametelegram";
+const gameName = "FirstGameTelegramBot";
 const queries = {};
-server.use(express.static(path.join(__dirname, 'firstgametelegram')));
+server.use(express.static(path.join(__dirname, 'FirstGameTelegramBot')));
 bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Oynamak istiyorsan /game de."));
 bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
 bot.on("callback_query", function (query) {
